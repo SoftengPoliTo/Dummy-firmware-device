54e3278f00189d278342d1eb4c6952843be927a3
Giuseppe <white951@hotmail.it>
2024-01-25 13:55:14 +0100

    Update deploy

--- libfakedevice-c/autorun.sh
+++ libfakedevice-c/autorun.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Create evetually the folder to contain the build
+mkdir -p ../binaries/libfakedevice-c
+
 # Configure and compile the project with Meson and Ninja
 echo "Project configuration and compilation..."
 env CC=clang meson setup build
 ninja -C build
+
+# Copy the produced build to the binary folder
+cp -r build/** ../binaries/libfakedevice-c/
